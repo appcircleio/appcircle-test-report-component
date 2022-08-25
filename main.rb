b7576a124d5883fa0663c75654e400277f37460e
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-08-25 13:50:52 +0300

    Don't parse tests or coverage if the path is empty

--- main.rb
+++ main.rb
@@ -15,13 +15,27 @@ puts "Platform #{platform}"
 report = {}
 
 if platform == 'ObjectiveCSwift'
-  parser = XcodeParser.new(test_path, repo_path, output_path)
-  report = parser.parse
+  if test_path.nil? || test_path.empty?
+    puts 'No test path given'
+  else
+    parser = XcodeParser.new(test_path, repo_path, output_path)
+    report = parser.parse
+  end
 else
-  test_parser = TestParser.new(test_path)
-  test_suites = test_parser.parse
-  coverage_parser = CoverageParser.new(coverage_path)
-  coverage = coverage_parser.parse
+  test_suites = {}
+  coverage = {}
+  if test_path.nil? || test_path.empty?
+    puts 'No test path given'
+  else
+    test_parser = TestParser.new(test_path)
+    test_suites = test_parser.parse
+  end
+  if coverage_path.nil? || coverage_path.empty?
+    puts 'No coverage path given'
+  else
+    coverage_parser = CoverageParser.new(coverage_path)
+    coverage = coverage_parser.parse
+  end
   report = { coverage: coverage, test_suites: test_suites }
 end
 
@@ -29,6 +43,6 @@ File.open("#{output_path}/test_results.json", 'w') do |f|
   f.write(JSON.pretty_generate(report))
 end
 
-File.open(ENV['AC_ENV_FILE_PATH'], 'a') { |f|
+File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
   f.puts "AC_TEST_REPORT_JSON_PATH=#{output_path}/test_results.json"
-}
+end
