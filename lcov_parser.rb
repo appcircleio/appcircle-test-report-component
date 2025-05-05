4bd4164b15aee124168a21d7b63acb31f0fd0ada
boztopuz <93190375+boztopuz@users.noreply.github.com>
2025-05-05 14:54:09 +0300

    [BE-4788]: Fixed SF parameter if the TN parameter is empty, parse the test name as SF (#3)
    * [BE-4788]: Fixed SF parameter if the TN parameter is empty, parse the test name as SF
    
    * changed handled file base name
    
    * [BE-4788]: removed variable that used one time, and change variable logic
    
    * [BE-4993]: Added -legacy flag for xcode cli  xcresulttool command (#4)
    
    * [BE-4993]: Added -legacy flag for xcode cli  xcresulttool command
    
    * [BE-4993]: Refactor some function, and handled multiple execute of xcode version
    
    * [BE-4993]: deleted unexpeted argument
    
    * [BE-4993]: fixed wrong logic of cmd function
    
    * [BE-4993]: Fixed wrong version increament
    
    * [BE-4993]: Fixed duplicated logics

--- lcov_parser.rb
+++ lcov_parser.rb
@@ -45,11 +45,13 @@ class LcovParser
                         executableLines: 0,
                         lineCoverage: 0,
                         functions: [] }
-        target[:name] = Regexp.last_match(1).strip.empty? ? "No Name" : Regexp.last_match(1).strip
+        tn_value = Regexp.last_match(1).strip
+        target[:name] = tn_value.empty? ? "No Name" : tn_value
 
       when /^SF:(.+)/ # Source file
         current_filename = $LAST_MATCH_INFO[1].gsub(%r{^\./}, "")
         target_file[:name] = current_filename
+        target[:name] = File.basename(current_filename) if target[:name] == "No Name"
       when /^LF:(\d+)/ # Line execution count
         total = $LAST_MATCH_INFO[1]
         target_file[:executableLines] = total.to_i
